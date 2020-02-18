8e21ee80231e7620d130f08a1949117c2d478b8b
luckyleonard <dzhu31@hotmail.com>
2020-02-18 23:09:49 +1100

    Updates

---
+++ precache-manifest.036853c44043be1165458e4c6461d1bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d61fb414fea5edbc5b685937b0acb17",
+    "url": "/React-TodoList/index.html"
+  },
+  {
+    "revision": "14868679cf45550f8e5c",
+    "url": "/React-TodoList/static/css/main.7145e2f7.chunk.css"
+  },
+  {
+    "revision": "2e5d9bbb9759eb2356ef",
+    "url": "/React-TodoList/static/js/2.506e7d92.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-TodoList/static/js/2.506e7d92.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14868679cf45550f8e5c",
+    "url": "/React-TodoList/static/js/main.9d3b8513.chunk.js"
+  },
+  {
+    "revision": "9d142fa3d6b74f7280ce",
+    "url": "/React-TodoList/static/js/runtime-main.35f60881.js"
+  }
+]);
